6b37ebb2d33f9aef2974707936b375992b4af2aa
Rahul Ghosh <ghrahul2016@gmail.com>
2018-03-28 16:54:24 +0530

    running two camera simultaneo usly for taking photos

--- webcamapp/Form1.Designer.cs
+++ webcamapp/Form1.Designer.cs
@@ -32,14 +32,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(336, 258);
+            this.pictureBox1.Size = new System.Drawing.Size(176, 258);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -47,9 +51,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 304);
+            this.button1.Location = new System.Drawing.Point(3, 321);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.Size = new System.Drawing.Size(176, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,9 +61,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(273, 304);
+            this.button3.Location = new System.Drawing.Point(181, 321);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 38);
+            this.button3.Size = new System.Drawing.Size(176, 38);
             this.button3.TabIndex = 3;
             this.button3.Text = "STOP";
             this.button3.UseVisualStyleBackColor = true;
@@ -68,16 +72,44 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 277);
+            this.comboBox1.Location = new System.Drawing.Point(3, 294);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(345, 21);
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
             this.comboBox1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(181, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(176, 258);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(181, 294);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(3, 268);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 20);
+            this.textBox1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 353);
+            this.ClientSize = new System.Drawing.Size(360, 365);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
@@ -86,7 +118,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +130,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- webcamapp/Form1.cs
+++ webcamapp/Form1.cs
@@ -16,59 +16,90 @@ namespace webcamapp
         }
         private FilterInfoCollection VideoCaptureDevices;
         private VideoCaptureDevice FinalVideo;
+        private VideoCaptureDevice FinalVideo1;
+        int stop = 0; int j = 1;
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
             {
                 comboBox1.Items.Add(VideoCaptureDevice.Name);
+                comboBox2.Items.Add(VideoCaptureDevice.Name);
             }
             comboBox1.SelectedIndex = 0;
-
+            comboBox2.SelectedIndex = 1;
+            textBox1.Visible = false;
         }
-
         private async void button1_Click(object sender, EventArgs e)
         {
+            textBox1.Visible = false;
+            //CAMERA 1
             FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
             FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
             FinalVideo.Start();
-            //Thread.Sleep(5000);
-            await System.Threading.Tasks.Task.Delay(10000);
-            autoClick();
+            //CAMERA 2
+            FinalVideo1 = new VideoCaptureDevice(VideoCaptureDevices[comboBox2.SelectedIndex].MonikerString);
+            FinalVideo1.NewFrame += new NewFrameEventHandler(FinalVideo1_NewFrame);
+            FinalVideo1.Start();
+            await System.Threading.Tasks.Task.Delay(5000);
+            stop = 0;
+            autoClick1();//first page
+            textBox1.Visible = false;
+            await System.Threading.Tasks.Task.Delay(2000);
+            autoClick();//camera auto
         }
         void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             eventArgs.Frame.RotateFlip(RotateFlipType.Rotate180FlipY);
             Bitmap video = (Bitmap)eventArgs.Frame.Clone();
             pictureBox1.Image = video;
-
         }
-
+        void FinalVideo1_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            eventArgs.Frame.RotateFlip(RotateFlipType.Rotate180FlipY);
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox2.Image = video;
+        }
         private void button3_Click(object sender, EventArgs e)
         {
             if (FinalVideo.IsRunning == true)
-            {
                 FinalVideo.SignalToStop();
-            }
-
+            if (FinalVideo1.IsRunning == true)
+                FinalVideo1.SignalToStop();
+            stop = 1;
+            autoClick2();//last page
         }
-
         private async void autoClick()
         {
-            int j = 1;
-            while (j!=999)
+           
+            while (stop==0)
             {
-                
+                textBox1.Visible = true;
                 pictureBox1.Image.Save(@"F:\picture\" + j.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-                await System.Threading.Tasks.Task.Delay(10000);
                 j++;
+                textBox1.Visible = true;
+                pictureBox2.Image.Save(@"F:\picture\" + j.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                j++;
+                textBox1.Visible = false;
+                await System.Threading.Tasks.Task.Delay(2000);
             }
+        }  
+        private void autoClick1()//first_page
+        {
+            textBox1.Visible = true;
+            pictureBox2.Image.Save(@"F:\picture\" + j.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            j++;
+        }
+        private void autoClick2()//last_page
+        {
+            textBox1.Visible = true;
+            pictureBox1.Image.Save(@"F:\picture\" + j.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            j++;
         }
     }
 }
